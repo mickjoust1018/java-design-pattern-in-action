f45408ba7ed793a4a10fed5415b7620e24aa7b77
mickjoust <mickjoust@163.com>
2021-03-10 15:23:54 +0800

    fix

--- jdp-in-action-dip/src/main/java/com/mickjoust/demo/jdp/chp2/a09/PaperBoy.java
+++ jdp-in-action-dip/src/main/java/com/mickjoust/demo/jdp/chp2/a09/PaperBoy.java
@@ -4,5 +4,13 @@ package com.mickjoust.demo.jdp.chp2.a09;
  * @author mickjoust
  **/
 public class PaperBoy {
-
+    public void charge(Customer myCustomer, float payment) {
+        payment = 2f;
+        Wallet theWallet = myCustomer.getWallet();
+        if (theWallet.getTotalMoney() > payment) {
+            theWallet.subtractMoney(payment);
+        } else {
+            //钱不够的处理
+        }
+    }
 }
