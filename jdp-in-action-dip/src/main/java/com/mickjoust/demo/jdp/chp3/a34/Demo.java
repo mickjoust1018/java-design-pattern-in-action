f32638254d01d6e043d085c9fd30039021ce65fb
mickjoust <mickjoust@163.com>
2021-05-30 00:01:14 +0800

    fix

---
+++ jdp-in-action-dip/src/main/java/com/mickjoust/demo/jdp/chp3/a34/Demo.java
@@ -0,0 +1,7 @@
+package com.mickjoust.demo.jdp.chp3.a34;
+
+/**
+ * @author mickjoust
+ **/
+public class Demo {
+}
---
+++ jdp-in-action-dip/src/main/java/com/mickjoust/demo/jdp/chp3/a36/Demo.java
@@ -0,0 +1,7 @@
+package com.mickjoust.demo.jdp.chp3.a36;
+
+/**
+ * @author mickjoust
+ **/
+public class Demo {
+}
